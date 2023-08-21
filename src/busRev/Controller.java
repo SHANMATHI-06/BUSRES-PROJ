f2da9abadb2179427f09a7f71eb619407cfe1fb8
SHANMATHI <shanmathi663@gmail.com>
2023-08-21 10:19:07 +0530

    Initial commit

---
+++ src/busRev/Controller.java
@@ -0,0 +1,33 @@
+package busRev;
+import java.io.BufferedOutputStream;
+import java.sql.SQLException;
+import java.util.Scanner; //nested package/ hierarchical package
+import java.util.ArrayList;
+public class Controller {
+
+    public static void main(String[] args) throws SQLException {
+        BusDAO busdao = new BusDAO();
+        try {
+            busdao.displayBusInfo();
+            int userOpt = 1;
+            Scanner scanner = new Scanner(System.in);
+            while (userOpt == 1) {
+                System.out.println("Enter 1 to Book and 2 to exit");
+                userOpt = scanner.nextInt();
+                if (userOpt == 1) {
+                    Booking booking = new Booking();
+                    if (booking.isAvailable()) {
+                        BookingDAO bookingdao = new BookingDAO();
+                        bookingdao.addBooking(booking);
+                        System.out.println("Your booking is confirmed");
+                    } else
+                        System.out.println("Sorry. Bus is full. Try another bus or date.");
+                }
+            }
+            scanner.close();
+        } catch (Exception e) {
+            System.out.println(e);
+
+        }
+    }
+}
